be5d6e4a8f56021ef0179d2146afed9ec63806c9
Pedram Navid <pedram.navid@gmail.com>
2023-01-16 22:04:39 -0800

    test table args for run_command

--- tests/dbtpal_spec.lua
+++ tests/dbtpal_spec.lua
@@ -1,15 +1,3 @@
-function P(...)
-    local objects = {}
-    for i = 1, select('#', ...) do
-        local v = select(i, ...)
-        table.insert(objects, vim.inspect(v))
-    end
-
-    print(table.concat(objects, '\n'))
-    return ...
-end
-
-
 describe("dbtpal", function()
     it('can be required', function()
         require('dbtpal')
@@ -18,14 +6,13 @@ describe("dbtpal", function()
     it('can run dbt', function()
         local dbt = require('dbtpal')
         local result = dbt.run()
-        P(result)
         assert.are.equal(result.command, "dbt")
         assert.are.equal(result.args[3], "run")
     end)
 
     it('can run standard dbt commands', function()
         local dbt = require('dbtpal')
-        commands = {'run', 'test', 'compile', 'build', 'debug'}
+        commands = { 'run', 'test', 'compile', 'build', 'debug' }
         for _, cmd in ipairs(commands) do
             local result = dbt.run_command(cmd)
             assert.are.equal(result.command, "dbt")
@@ -33,13 +20,26 @@ describe("dbtpal", function()
         end
     end)
 
-    it('can arbitrary commands', function()
+    it('can arbitrary commands with string args', function()
         local dbt = require('dbtpal')
+
         local result = dbt.run_command("compile", "--models my_model")
         assert.are.equal(result.command, "dbt")
         assert.are.equal(result.args[3], "compile")
         assert.are.equal(result.args[4], "--models")
         assert.are.equal(result.args[5], "my_model")
+    end)
+
+
+    it('can arbitrary commands with table args', function()
+        local dbt = require('dbtpal')
+        local result = dbt.run_command("compile", { "--models", "my_model" })
+        assert.are.equal(result.command, "dbt")
+        assert.are.equal(result.args[3], "compile")
+        assert.are.equal(result.args[4], "--models")
+        assert.are.equal(result.args[5], "my_model")
+
+
     end)
 
 end)
